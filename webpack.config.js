10a7306ac1dd999e45c51cfba6d19113ee8d020d
naoya_takahashi <49216915+naoya-takahashi@users.noreply.github.com>
2020-09-10 12:58:44 +0900

    セクション6 clean-webpack-pluginの追加&ファイル構成変更

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/js/main.js',
   output: {
     // 相対パスだとerrorとなる
     path: path.resolve(__dirname, './dist'),
-    filename: 'main.js'
+    filename: './js/main.js'
   },
   module: {
     rules: [
@@ -28,9 +29,12 @@ module.exports = {
     ],
   },
   plugins: [
-    new MiniCssExtractPlugin(),
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
+    new MiniCssExtractPlugin({
+      filename: './css/style.css',
     }),
+    new HtmlWebpackPlugin({
+      template: './src/templates/index.html',
+    }),
+    new CleanWebpackPlugin(),
   ],
 }
