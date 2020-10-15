f2bfe617d457c9cd01a1269476fc6409837d076f
naoya_takahashi <49216915+naoya-takahashi@users.noreply.github.com>
2020-10-15 09:14:44 +0900

    セクション8 効率的なHTMLの制作方法（pugの導入）

--- webpack.config.js
+++ webpack.config.js
@@ -46,6 +46,20 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.pug/,
+        use:[
+          {
+            loader: 'html-loader',
+          },
+          {
+            loader: 'pug-html-loader',
+            options: {
+              pretty: true,
+            },
+          },
+        ],
+      },
     ],
   },
   plugins: [
@@ -53,7 +67,12 @@ module.exports = {
       filename: './css/style.css',
     }),
     new HtmlWebpackPlugin({
-      template: './src/templates/index.html',
+      template: './src/templates/index.pug',
+      filename: 'index.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/templates/access.pug',
+      filename: 'access.html',
     }),
     new CleanWebpackPlugin(),
   ],
